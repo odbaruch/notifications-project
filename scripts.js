8dbce11587b0007e3ea0c27446b84f8a583252e6
Omer Baruch <odbaruch@gmail.com>
2022-10-11 14:39:08 +0300

    Add event listeners for single notification clicks

--- scripts.js
+++ scripts.js
@@ -1,8 +1,16 @@
 const notificationsNumber = document.querySelector(".notifications-number");
 const unreadNotifications = document.querySelectorAll(".unread");
+const notificationLinks = document.querySelectorAll(".name, .group, .post, .notification-message");
 function clearNotifications() {
     notificationsNumber.innerHTML = "0";
     for (let i = 0; i < unreadNotifications.length; i++) {
         unreadNotifications[i].classList.remove("unread");
-    }
-}
+    };
+};
+for (let i = 0; i < notificationLinks.length; i++) {
+    notificationLinks[i].addEventListener('click', function() {
+        notificationLinks[i].closest(".notification-card").classList.remove("unread");
+        var unreadNumber = document.querySelectorAll(".unread").length;
+        notificationsNumber.innerHTML = unreadNumber;
+    }, {once : true});
+};
